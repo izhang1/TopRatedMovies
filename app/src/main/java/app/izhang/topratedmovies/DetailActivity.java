65ef1c1ef6dbdc163e528e2956cf5179d847c9d6
Ivan Zhang <ivanzhangemail@gmail.com>
2017-09-17 12:46:56 -0400

    Initial Data Confirmation in DetailView
    * Confirmed that the data is being passed through from the HomeView
    
    * Confirmed that the data is correct
    
    * Setup initial TextViews that will be used for layout of the UI

--- app/src/main/java/app/izhang/topratedmovies/DetailActivity.java
+++ app/src/main/java/app/izhang/topratedmovies/DetailActivity.java
@@ -2,10 +2,17 @@ package app.izhang.topratedmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import app.izhang.topratedmovies.data.Movie;
 
 public class DetailActivity extends AppCompatActivity {
+
+    public TextView mVoteAverageTV;
+    public TextView mOverviewTV;
+    public TextView mReleaseDateTV;
+
+    public Movie mMovieData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -14,9 +21,23 @@ public class DetailActivity extends AppCompatActivity {
         setContentView(R.layout.activity_detail);
 
         Bundle receivedData = getIntent().getExtras();
-        Movie currentMovie = (Movie) receivedData.get(getString(R.string.key_movie_object));
+        if(receivedData != null) {
+            mMovieData = (Movie) receivedData.get(getString(R.string.key_movie_object));
+        }
 
-        setTitle(currentMovie.getTitle());
+        setTitle(mMovieData.getTitle());
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        showUI();
+    }
+
+    public void showUI(){
+        mVoteAverageTV = (TextView) findViewById(R.id.tv_vote_average);
+        mOverviewTV = (TextView) findViewById(R.id.tv_overview);
+        mReleaseDateTV = (TextView) findViewById(R.id.tv_release_date);
+
+        mVoteAverageTV.setText("Vote Average: " + mMovieData.getVote_average());
+        mOverviewTV.setText("Overview: " + mMovieData.getOverview());
+        mReleaseDateTV.setText("Release Date: " + mMovieData.getRelease_date());
     }
 }

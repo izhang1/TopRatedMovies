ed5c9c7a24c01aa46546d9c93d89f133b144f751
Ivan Zhang <ivanzhangemail@gmail.com>
2017-09-17 12:56:21 -0400

    DetailView UI Implementation
    * Setup initial implementation with poster image and data passed in
    from home view

--- app/src/main/java/app/izhang/topratedmovies/DetailActivity.java
+++ app/src/main/java/app/izhang/topratedmovies/DetailActivity.java
@@ -2,15 +2,21 @@ package app.izhang.topratedmovies;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.squareup.picasso.Picasso;
+
 import app.izhang.topratedmovies.data.Movie;
+import app.izhang.topratedmovies.utilities.PosterPathUtils;
 
 public class DetailActivity extends AppCompatActivity {
 
     public TextView mVoteAverageTV;
     public TextView mOverviewTV;
     public TextView mReleaseDateTV;
+
+    public ImageView mPosterView;
 
     public Movie mMovieData;
 
@@ -32,6 +38,11 @@ public class DetailActivity extends AppCompatActivity {
     }
 
     public void showUI(){
+
+        mPosterView = (ImageView) findViewById(R.id.poster_imageview);
+        String posterPath = PosterPathUtils.buildPosterURL(mMovieData.getPoster_path(), "size");
+        Picasso.with(this).load(posterPath).into(mPosterView);
+
         mVoteAverageTV = (TextView) findViewById(R.id.tv_vote_average);
         mOverviewTV = (TextView) findViewById(R.id.tv_overview);
         mReleaseDateTV = (TextView) findViewById(R.id.tv_release_date);
